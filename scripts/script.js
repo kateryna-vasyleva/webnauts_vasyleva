9e39a12493cf674e35050665a57cc4ec4e1585b9
Kateryna Vasyleva <kateryna.vasyleva@gmail.com>
2021-07-06 07:21:44 +0300

    scrypt

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
 let popUp=document.querySelector('.pop_up');
-let popUpBtn=document.querySelector('.btn_pop_up')
+let btn=document.querySelector(".btn_dark");
+let btnClose=document.querySelector('.pop_up_close');
+let btnStartScreen=document.querySelector ('.btn_pop_up');
+function showPopUpOnClick() {
+    popUp.classList.add('pop_up_show')
+}
+btn.addEventListener('click', showPopUpOnClick);
+btnStartScreen.addEventListener('click', showPopUpOnClick);
 
+btnClose.addEventListener('click', function() {
+    popUp.classList.remove('pop_up_show')
+})
